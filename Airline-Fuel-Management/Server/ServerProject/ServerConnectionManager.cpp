db9e7b19a761d64eae96239321c8dd75c9e39d43
[Caleb] <[ccullen8806036@conestogac.on.ca]>
2026-04-14 18:26:34 -0400

    telemetry

--- Airline-Fuel-Management/Server/ServerProject/ServerConnectionManager.cpp
+++ Airline-Fuel-Management/Server/ServerProject/ServerConnectionManager.cpp
@@ -3,8 +3,23 @@
 #include <thread>
 //#include <boost/asio/thread_pool.hpp>
 #include <boost/asio/post.hpp>
+#include <mutex>
+#include <unordered_map>
+#include <fstream>
+#include <ctime>
+
+#pragma warning(disable : 4996)
 
 using namespace std;
+
+struct FlightState {
+    double initialFuel = -1;
+    double lastFuel = -1;
+    long long lastTime = -1;
+};
+
+static std::unordered_map<std::string, FlightState> flightStates;
+static std::mutex flightStatesMutex;
 
 ServerConnectionManager::ServerConnectionManager(int serverPort, TaskScheduler& scheduler) 
     : port(serverPort), WelcomeSocket(INVALID_SOCKET), isRunning(false), scheduler(scheduler), connectionPool(100) {}
@@ -140,6 +155,20 @@ void ServerConnectionManager::handleClientSession(SOCKET ConnectionSocket, const
         string chunk(rxBuffer, bytesReceived);
         if (chunk.find("FLIGHT_COMPLETE") != string::npos) {
             cout << "[" << clientID << "] Flight complete." << endl;
+            // post final average calculation to scheduler
+            scheduler.enqueueTask([clientID]() {
+                std::lock_guard<std::mutex> lock(flightStatesMutex);
+                auto it = flightStates.find(clientID);
+                if (it != flightStates.end()) {
+                    double totalFuelConsumed = it->second.initialFuel - it->second.lastFuel;
+                    std::ofstream outFile("telemetry_Client" + clientID + ".txt", std::ios::app);
+                    if (outFile.is_open()) {
+                        outFile << "FLIGHT_COMPLETE: Total Fuel Consumed: " << totalFuelConsumed << "\n";
+                    }
+                    cout << "[" << clientID << "] Final total fuel consumed: " << totalFuelConsumed << endl;
+                    flightStates.erase(it);
+                }
+            });
             break;
         }
 
@@ -150,16 +179,42 @@ void ServerConnectionManager::handleClientSession(SOCKET ConnectionSocket, const
 
             // post telemetry processing to task scheduler
             scheduler.enqueueTask([packet, clientID]() {
-                // insert telemetry processing function
-
-
                 // telemetry log to console
                 cout << "[" << clientID << "] "
                     << "ts=" << packet.timestamp
                     << " fuel=" << packet.fuel
                     << endl;
+                    
+                std::lock_guard<std::mutex> lock(flightStatesMutex);
+                auto& state = flightStates[clientID];
+                
+                long long currentSeconds = 0;
+                int m=0, d=0, y=0, h=0, min=0, s=0;
+                if (sscanf(packet.timestamp.c_str(), "%d_%d_%d %d:%d:%d", &m, &d, &y, &h, &min, &s) == 6) {
+                    struct tm tm = {0};
+                    tm.tm_year = y - 1900;
+                    tm.tm_mon = m - 1;
+                    tm.tm_mday = d;
+                    tm.tm_hour = h;
+                    tm.tm_min = min;
+                    tm.tm_sec = s;
+                    currentSeconds = mktime(&tm);
+                }
 
-				});
+                if (state.initialFuel < 0) {
+                    state.initialFuel = packet.fuel;
+                    state.lastFuel = packet.fuel;
+                    state.lastTime = currentSeconds;
+                } else {
+                    double fuelConsumed = state.lastFuel - packet.fuel; // per interval consumption
+                    std::ofstream outFile("telemetry_Client" + clientID + ".txt", std::ios::app);
+                    if (outFile.is_open()) {
+                        outFile << "ts=" << packet.timestamp << " fuel_consumed=" << fuelConsumed << "\n";
+                    }
+                    state.lastFuel = packet.fuel;
+                    state.lastTime = currentSeconds;
+                }
+            });
         }
     }
 
